d34676c24c5ead4bdd067ade729020e921e85ad5
the733 <103292422+the733@users.noreply.github.com>
2022-04-11 15:09:23 -0300

    Delete updates directory

--- updates/pwn_pad_patch_0.2.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "Removing Ruby 1.8 and installing Metasploit bundle"
-apt-get remove ruby1.8 ruby1.8-dev libruby1.8 rubygems
-gem install bundler --no-ri --no-rdoc
-cd /opt/metasploit-framework && bundle install
-echo "Updating Metasploit"
-msfupdate
